d4fee2a7843998b64382a98399b366a5a108c47b
I-am-Seeann <rusetski.ana@gmail.com>
2025-12-15 23:05:37 +0400

    modified cat_api and ignore file

--- cat_api.py
+++ cat_api.py
@@ -7,13 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_random_cat(logger):
-    api_key = os.environ.get('CAT_API_KEY')
-
     try:
         url = "https://api.thecatapi.com/v1/images/search"
-        headers = {'x-api-key': api_key} if api_key else {}
 
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url=url, timeout=10)
         response.raise_for_status()
 
         data = response.json()
